0dc66af4f61ede220e53f81b85ea782c6eefcaef
Shoaib-CS <shoaibcs252@gmail.com>
2024-07-08 21:17:03 +0500

    Added Chapter1

---
+++ Chapter1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+ int main()
+{
+    system("Color 04");
+    cout << " ####################################################################################" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #       Score: 150       #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #       Health: 5        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #        Lives: 3        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " #                                                         #                        #" << endl;
+    cout << " ####################################################################################" << endl;
+return 0;
+}
